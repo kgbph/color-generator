7628f0c30390795cd569ca36cc007bbfdd7b6046
Kevin Bernardo <kgb.acct.personal@gmail.com>
2020-06-07 22:45:51 +0800

    update test page

--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
 $(document).ready(function() {
-    $('#btnGenerateColor').click(function() {
+    $('#frmGenerateColor').submit(function(e) {
+        e.preventDefault();
+
         let colorGenerator = new ColorGenerator();
         let sampleCount = $('#inpSampleCount').val();
         let template = $('#divColorSample').html();
         let $sandBox = $('#divSandbox');
         let $generatedSet = $();
+
         for (let i = 0; i < sampleCount; i++) {
-            let generatedColor = colorGenerator.generateColor();
+            let generatedColor = colorGenerator.generateHex();
             let $generatedObject = $(template);
             $('.card-body', $generatedObject).css('background-color', generatedColor);
             $('.card-title', $generatedObject).text(generatedColor);
             $generatedSet = $generatedSet.add($generatedObject);
         }
+
         $sandBox.html($generatedSet);
     });
 });
